5084c798aa697fb90a9226d09fc496bf63b8669c
vivek <boravivek459@gmail.com>
2026-09-11 11:18:39 +0100

    made changes in dsicord file

--- client/src/discordSdk.js
+++ client/src/discordSdk.js
@@ -3,6 +3,7 @@ import { DiscordSDK } from "@discord/embedded-app-sdk";
 const discordSdk = new DiscordSDK("1547857715450093568");
 
 export const initializeDiscord = async () => {
+      console.log("Initializing Discord SDK...");
   try {
     // Wait for Discord to establish the connection
     await discordSdk.ready();
