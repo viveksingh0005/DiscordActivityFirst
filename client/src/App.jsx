f921d89c0a14ec55d7473872f7602f2f5f2eefe7
vivek <boravivek459@gmail.com>
2026-09-11 12:30:00 +0100

    made new changes to fix bug

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,6 @@
 import RandomNumberGenerator from "./components/RandomNumberGenerator"
 import { useEffect } from "react";
-import { initializeDiscord } from "./discordSDK";
+import { initializeDiscord } from "./discordSdk";
 function App() {
   
 
